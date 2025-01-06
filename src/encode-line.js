9f0249a25a0623dcf83c9c20367045c996f512aa
Mila Balashova <mila.balashova2312@gmail.com>
2025-01-07 01:17:15 +0300

    add solutions for tasks 7-8

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str.length === 0) return '';
+
+  let compressed = '';
+  let count = 1;
+
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count += 1;
+    } else {
+      if (count > 1) {
+        compressed += count;
+      }
+      compressed += str[i - 1];
+      count = 1;
+    }
+  }
+
+  return compressed;
 }
 
 module.exports = {
