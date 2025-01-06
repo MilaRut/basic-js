5e403543339524d94ea533b42605eac0404a9358
Mila Balashova <mila.balashova2312@gmail.com>
2025-01-07 02:13:57 +0300

    add solutions for tasks 19-21

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+    const keyLength = key.length;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+
+      if (char.match(/[A-Z]/)) {
+        const messageCharCode = char.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCharCode = key[keyIndex % keyLength].charCodeAt(0) - 'A'.charCodeAt(0);
+        const encryptedChar = String.fromCharCode(((messageCharCode + keyCharCode) % 26) + 'A'.charCodeAt(0));
+        result += encryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (typeof encryptedMessage !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+    const keyLength = key.length;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const char = encryptedMessage[i];
+
+      if (char.match(/[A-Z]/)) {
+        const encryptedCharCode = char.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCharCode = key[keyIndex % keyLength].charCodeAt(0) - 'A'.charCodeAt(0);
+        const decryptedChar = String.fromCharCode(((encryptedCharCode - keyCharCode + 26) % 26) + 'A'.charCodeAt(0));
+        result += decryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
